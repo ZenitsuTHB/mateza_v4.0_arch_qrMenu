5ce1fb78799309d3f10a49c29f73bc12d6f302ac
thibaultvandesompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-05 21:06:35 +0200

    RUn

--- src/Pages/Account/AvatarSelection.js
+++ src/Pages/Account/AvatarSelection.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { withHeader } from '../../Components/Structural/Header/index.js';
-import './css/avatarSelection.css'; // Import the CSS for styling
+import React, { useState, useRef } from 'react';
+import './css/avatarSelection.css';
 
 // Importing avatar images
 import blue1 from '../../Assets/avatars/blue1.png';
@@ -27,8 +26,30 @@ const avatars = [
 
 const AvatarSelection = ({ onSelectAvatar }) => {
   const [selectedAvatar, setSelectedAvatar] = useState(null);
+  const [avatarPosition, setAvatarPosition] = useState({ initialTop: 0, initialLeft: 0 });
+  const [avatarTransform, setAvatarTransform] = useState({ deltaX: 0, deltaY: 0 });
+  const avatarRefs = useRef([]);
 
   const handleSelectAvatar = (index) => {
+    const avatarElement = avatarRefs.current[index];
+    const rect = avatarElement.getBoundingClientRect();
+    const initialTop = rect.top;
+    const initialLeft = rect.left;
+
+    const avatarWidth = avatarElement.offsetWidth;
+    const avatarHeight = avatarElement.offsetHeight;
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const centerLeft = viewportWidth / 2 - avatarWidth / 2;
+    const centerTop = viewportHeight / 2 - avatarHeight / 2;
+
+    const deltaX = centerLeft - initialLeft;
+    const deltaY = centerTop - initialTop;
+
+    setAvatarPosition({ initialTop: initialTop, initialLeft: initialLeft });
+    setAvatarTransform({ deltaX: deltaX, deltaY: deltaY });
     setSelectedAvatar(index);
     onSelectAvatar(index);
   };
@@ -41,10 +62,34 @@ const AvatarSelection = ({ onSelectAvatar }) => {
           {avatars.map((avatar, index) => (
             <button
               key={index}
-              className={`avatar-button ${selectedAvatar === index ? 'selected' : ''}`}
+              className={`avatar-button ${
+                selectedAvatar === index
+                  ? 'selected'
+                  : selectedAvatar !== null
+                  ? 'unselected'
+                  : ''
+              }`}
               onClick={() => handleSelectAvatar(index)}
+              ref={(el) => (avatarRefs.current[index] = el)}
+              style={
+                selectedAvatar === index
+                  ? {
+                      position: 'fixed',
+                      top: avatarPosition.initialTop,
+                      left: avatarPosition.initialLeft,
+                      animation: 'moveToCenter 1s forwards',
+                      '--deltaX': `${avatarTransform.deltaX}px`,
+                      '--deltaY': `${avatarTransform.deltaY}px`,
+                      zIndex: 10,
+                    }
+                  : {}
+              }
             >
-              <img src={avatar} alt={`Avatar ${index + 1}`} className="avatar-image" />
+              <img
+                src={avatar}
+                alt={`Avatar ${index + 1}`}
+                className={`avatar-image ${selectedAvatar === index ? 'miraculous' : ''}`}
+              />
             </button>
           ))}
         </div>
@@ -62,4 +107,3 @@ const AvatarSelection = ({ onSelectAvatar }) => {
 };
 
 export default AvatarSelection;
-
