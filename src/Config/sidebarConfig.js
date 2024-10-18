bc1f5d8815e689fcd8cd6bf64eea59bea2f4e57a
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-18 03:41:49 +0200

    RUn

--- src/Config/sidebarConfig.js
+++ src/Config/sidebarConfig.js
@@ -2,21 +2,13 @@
 
 import {
   FaChartPie,
-  FaColumns,
-  FaStickyNote,
   FaPencilAlt,
-  FaBoxes,
-  FaCalendarPlus,
 } from 'react-icons/fa';
 
 import DragAndDrop from '../Pages/FormEditor/index.js';
 import FormSettings from '../Pages/FormEditor/FormSettings/index.js';
 import LaunchPage from '../Pages/FormEditor/LaunchPage/index.js';
-import Notepad from '../Pages/Notepad/index.js';
 import Profile from '../Pages/Profile/index.js';
-import Pincode from '../Pages/Pincode/index.js';
-import NewReservation from '../Pages/NewReservation/index.js';
-import SplitScreen from '../Components/Structural/SplitScreen/index.js';
 
 const routesConfig = [
   {
@@ -92,42 +84,6 @@ const routesConfig = [
     label: 'Profiel',
     icon: FaChartPie,
     isMenu: false,
-    isMobile: false,
-    isTab: false,
-  },
-  {
-    path: '/pincode',
-    element: <Pincode title="Pincode" />,
-    label: 'Pincode',
-    icon: FaBoxes,
-    isMenu: true,
-    isMobile: true,
-    isTab: false,
-  },
-  {
-    path: '/notepad',
-    element: <Notepad title="Notities" />,
-    label: 'Notities',
-    icon: FaStickyNote,
-    isMenu: true,
-    isMobile: true,
-    isTab: false,
-  },
-  {
-    path: '/new-reservation',
-    element: <NewReservation title="Nieuwe Reservatie" />,
-    label: 'Reserveren',
-    icon: FaCalendarPlus,
-    isMenu: true,
-    isMobile: true,
-    isTab: true,
-  },
-  {
-    path: '/split',
-    element: <SplitScreen />,
-    label: 'Split View',
-    icon: FaColumns,
-    isMenu: true,
     isMobile: false,
     isTab: false,
   },
