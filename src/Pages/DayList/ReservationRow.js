f51d1078f60f8e2adba1cd0ef66e776a23a16ae4
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-19 02:36:42 +0200

    RUn

--- src/Pages/DayList/ReservationRow.js
+++ src/Pages/DayList/ReservationRow.js
@@ -1,30 +1,21 @@
-// src/components/ReservationsList/ReservationRow.jsx
-
-import React, { useState } from 'react';
-import Tooltip from './Tooltip';
-import { FaUsers } from 'react-icons/fa';
+import React from 'react';
+import { FaUsers, FaEllipsisV } from 'react-icons/fa';
 import './css/reservationRow.css';
 
 const ReservationRow = ({ reservation, activeTab }) => {
-  const [showNoteTooltip, setShowNoteTooltip] = useState(false);
-  const [showMenuTooltip, setShowMenuTooltip] = useState(false);
-
   return (
     <div className="reservation-row">
       <div className="reservation-number">
         <strong>{reservation.aantalGasten}</strong>
-        {(reservation.aantalGasten >= 5 && reservation.aantalGasten < 8) && (
+        {reservation.aantalGasten >= 5 && reservation.aantalGasten < 8 && (
           <FaUsers className="users-icon" />
         )}
-
-		{reservation.aantalGasten == 4 && (
+        {reservation.aantalGasten === 4 && (
           <FaUsers className="users-icon-gray" />
         )}
-
-		{reservation.aantalGasten >= 8 && (
+        {reservation.aantalGasten >= 8 && (
           <FaUsers className="users-icon-red" />
         )}
-		
       </div>
       <div>{reservation.tijdstip}</div>
       <div>
@@ -32,40 +23,8 @@ const ReservationRow = ({ reservation, activeTab }) => {
       </div>
       {activeTab !== 'eenvoudig' && <div>{reservation.email}</div>}
       {activeTab !== 'eenvoudig' && <div>{reservation.phone}</div>}
-      <div className="extra-actions">
-        {reservation.extra && (
-          <div
-            className="note-icon"
-            onMouseEnter={() => setShowNoteTooltip(true)}
-            onMouseLeave={() => setShowNoteTooltip(false)}
-          >
-            <i className="fa fa-sticky-note-o"></i>
-            {showNoteTooltip && (
-              <Tooltip>
-                <div className="tooltip-content">{reservation.extra}</div>
-              </Tooltip>
-            )}
-          </div>
-        )}
-        <div
-          className="menu-icon"
-          onClick={() => setShowMenuTooltip(!showMenuTooltip)}
-        >
-          <i className="fa fa-ellipsis-v"></i>
-          {showMenuTooltip && (
-            <Tooltip>
-              <div className="tooltip-item">
-                <i className="fa fa-edit"></i>
-                Bewerk
-              </div>
-              <div className="tooltip-separator"></div>
-              <div className="tooltip-item delete">
-                <i className="fa fa-trash"></i>
-                Verwijder
-              </div>
-            </Tooltip>
-          )}
-        </div>
+      <div className="extra-column">
+        <FaEllipsisV className="ellipsis-icon" />
       </div>
     </div>
   );
