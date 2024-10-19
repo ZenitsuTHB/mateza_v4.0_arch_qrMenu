b0e75bb47902a8ca3fe9b52b18b072ec6185071c
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-19 02:39:13 +0200

    RUn

--- src/Pages/DayList/ReservationRow.js
+++ src/Pages/DayList/ReservationRow.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import { FaUsers, FaEllipsisV } from 'react-icons/fa';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  FaUsers,
+  FaEllipsisV,
+  FaPencilAlt,
+  FaTrashAlt,
+} from 'react-icons/fa';
 import './css/reservationRow.css';
 
 const ReservationRow = ({ reservation, activeTab }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const tooltipTimerRef = useRef(null);
+
+  const handleMouseEnterIcon = () => {
+    // Clear any existing timer
+    if (tooltipTimerRef.current) {
+      clearTimeout(tooltipTimerRef.current);
+      tooltipTimerRef.current = null;
+    }
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeaveIcon = () => {
+    // Start timer to hide tooltip after 1.5 seconds
+    tooltipTimerRef.current = setTimeout(() => {
+      setShowTooltip(false);
+    }, 1500);
+  };
+
+  const handleMouseEnterTooltip = () => {
+    // Clear timer when entering tooltip
+    if (tooltipTimerRef.current) {
+      clearTimeout(tooltipTimerRef.current);
+      tooltipTimerRef.current = null;
+    }
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeaveTooltip = () => {
+    // Start timer to hide tooltip after 1.5 seconds
+    tooltipTimerRef.current = setTimeout(() => {
+      setShowTooltip(false);
+    }, 1500);
+  };
+
+  // Clean up timer on unmount
+  useEffect(() => {
+    return () => {
+      if (tooltipTimerRef.current) {
+        clearTimeout(tooltipTimerRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="reservation-row">
       <div className="reservation-number">
@@ -19,12 +68,37 @@ const ReservationRow = ({ reservation, activeTab }) => {
       </div>
       <div>{reservation.tijdstip}</div>
       <div>
-        <a href="#" className="name-link">{`${reservation.firstName} ${reservation.lastName}`}</a>
+        <a href="#" className="name-link">
+          {`${reservation.firstName} ${reservation.lastName}`}
+        </a>
       </div>
       {activeTab !== 'eenvoudig' && <div>{reservation.email}</div>}
       {activeTab !== 'eenvoudig' && <div>{reservation.phone}</div>}
       <div className="extra-column">
-        <FaEllipsisV className="ellipsis-icon" />
+        <div
+          className="ellipsis-container"
+          onMouseEnter={handleMouseEnterIcon}
+          onMouseLeave={handleMouseLeaveIcon}
+        >
+          <FaEllipsisV className="ellipsis-icon" />
+          {showTooltip && (
+            <div
+              className="tooltip-container"
+              onMouseEnter={handleMouseEnterTooltip}
+              onMouseLeave={handleMouseLeaveTooltip}
+            >
+              <div className="tooltip-item">
+                <FaPencilAlt className="tooltip-icon" />
+                Bewerken
+              </div>
+              <div className="tooltip-separator"></div>
+              <div className="tooltip-item">
+                <FaTrashAlt className="tooltip-icon" />
+                Verwijderen
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
