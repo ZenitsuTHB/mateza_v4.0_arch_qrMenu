55885340aa627692ce4b96deabf51433fb6449a1
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-27 19:24:30 +0100

    RUn

--- src/Pages/DayCalendar/Modal/Exceptions.js
+++ src/Pages/DayCalendar/Modal/Exceptions.js
@@ -1,7 +1,7 @@
 // src/components/Modal/ExceptionalDays.jsx
 
 import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import ExceptionItem from './ExceptionItems';
 import './css/exceptions.css';
 
 const ExceptionalDays = ({
@@ -55,64 +55,14 @@ const ExceptionalDays = ({
       <div className="exception-type-section">
         <h3>Sluitingsperiode</h3>
         {exceptionalDays.sluitingsperiode.map((item, index) => (
-          <div key={index} className="exceptional-day-item">
-            <div className="day-header">
-              <span className="day-label">Periode {index + 1}</span>
-              <label className="switch">
-                <input
-                  type="checkbox"
-                  checked={item.enabled}
-                  onChange={() => handleToggle('sluitingsperiode', index)}
-                />
-                <span className="slider round"></span>
-              </label>
-            </div>
-            <AnimatePresence>
-              {item.enabled && (
-                <motion.div
-                  className="inputs-container"
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: 'auto', opacity: 1 }}
-                  exit={{ height: 0, opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                  layout
-                >
-                  <label className="modal-label date-input">
-                    Start datum:
-                    <input
-                      type="date"
-                      value={item.startDate}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'sluitingsperiode',
-                          index,
-                          'startDate',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                  <label className="modal-label date-input">
-                    Eind datum:
-                    <input
-                      type="date"
-                      value={item.endDate}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'sluitingsperiode',
-                          index,
-                          'endDate',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
+          <ExceptionItem
+            key={index}
+            type="sluitingsperiode"
+            item={item}
+            index={index}
+            handleToggle={handleToggle}
+            handleInputChange={handleInputChange}
+          />
         ))}
         <button
           type="button"
@@ -127,48 +77,14 @@ const ExceptionalDays = ({
       <div className="exception-type-section">
         <h3>Sluitingsdag</h3>
         {exceptionalDays.sluitingsdag.map((item, index) => (
-          <div key={index} className="exceptional-day-item">
-            <div className="day-header">
-              <span className="day-label">Dag {index + 1}</span>
-              <label className="switch">
-                <input
-                  type="checkbox"
-                  checked={item.enabled}
-                  onChange={() => handleToggle('sluitingsdag', index)}
-                />
-                <span className="slider round"></span>
-              </label>
-            </div>
-            <AnimatePresence>
-              {item.enabled && (
-                <motion.div
-                  className="inputs-container"
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: 'auto', opacity: 1 }}
-                  exit={{ height: 0, opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                  layout
-                >
-                  <label className="modal-label date-input">
-                    Datum:
-                    <input
-                      type="date"
-                      value={item.date}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'sluitingsdag',
-                          index,
-                          'date',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
+          <ExceptionItem
+            key={index}
+            type="sluitingsdag"
+            item={item}
+            index={index}
+            handleToggle={handleToggle}
+            handleInputChange={handleInputChange}
+          />
         ))}
         <button
           type="button"
@@ -183,82 +99,14 @@ const ExceptionalDays = ({
       <div className="exception-type-section">
         <h3>Uitzonderlijke Openingsuren</h3>
         {exceptionalDays.uitzonderlijkeOpeningsuren.map((item, index) => (
-          <div key={index} className="exceptional-day-item">
-            <div className="day-header">
-              <span className="day-label">Uur {index + 1}</span>
-              <label className="switch">
-                <input
-                  type="checkbox"
-                  checked={item.enabled}
-                  onChange={() =>
-                    handleToggle('uitzonderlijkeOpeningsuren', index)
-                  }
-                />
-                <span className="slider round"></span>
-              </label>
-            </div>
-            <AnimatePresence>
-              {item.enabled && (
-                <motion.div
-                  className="inputs-container"
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: 'auto', opacity: 1 }}
-                  exit={{ height: 0, opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                  layout
-                >
-                  <label className="modal-label date-input">
-                    Datum:
-                    <input
-                      type="date"
-                      value={item.date}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'uitzonderlijkeOpeningsuren',
-                          index,
-                          'date',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                  <label className="modal-label time-input">
-                    Start tijd:
-                    <input
-                      type="time"
-                      value={item.startTime}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'uitzonderlijkeOpeningsuren',
-                          index,
-                          'startTime',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                  <label className="modal-label time-input">
-                    Eindtijd:
-                    <input
-                      type="time"
-                      value={item.endTime}
-                      onChange={(e) =>
-                        handleInputChange(
-                          'uitzonderlijkeOpeningsuren',
-                          index,
-                          'endTime',
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                  </label>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
+          <ExceptionItem
+            key={index}
+            type="uitzonderlijkeOpeningsuren"
+            item={item}
+            index={index}
+            handleToggle={handleToggle}
+            handleInputChange={handleInputChange}
+          />
         ))}
         <button
           type="button"
