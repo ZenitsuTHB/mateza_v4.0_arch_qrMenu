b9fc751cbe66aa7f480fe92f67eaf6030e8008e7
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-10-31 00:33:41 +0100

    RUn

--- src/Pages/DayCalendar/Modal/Scheme/SchemeItem.js
+++ src/Pages/DayCalendar/Modal/Scheme/SchemeItem.js
@@ -25,6 +25,8 @@ const SchemeItem = ({
   useEffect(() => {
     if (enabled) {
       setIsExpanded(true);
+    } else {
+      setIsExpanded(false);
     }
   }, [enabled]);
 
@@ -33,10 +35,16 @@ const SchemeItem = ({
       key={item.id}
       className={`scheme-item ${item.type !== 'day' ? 'scheme-item-special' : ''}`}
     >
-      {/* Item header with label and arrow icon */}
+      {/* Item header with label and toggle switch */}
       <div className="item-header">
-        <div className="item-label" onClick={() => setIsExpanded(!isExpanded)}>
-          <FaChevronDown className={`arrow-icon ${isExpanded ? 'expanded' : ''}`} />
+        <div
+          className={`item-label ${!enabled ? 'disabled' : ''}`}
+          onClick={enabled ? () => setIsExpanded(!isExpanded) : null}
+        >
+          {/* Conditionally render the chevron icon when enabled */}
+          {enabled && (
+            <FaChevronDown className={`arrow-icon ${isExpanded ? 'expanded' : ''}`} />
+          )}
           {item.label}
         </div>
         {/* ToggleSwitch remains on the right */}
@@ -47,7 +55,7 @@ const SchemeItem = ({
       </div>
 
       <AnimatePresence>
-        {isExpanded && (
+        {isExpanded && enabled && (
           <motion.div
             className="item-content"
             initial={{ height: 0, opacity: 0 }}
@@ -56,83 +64,80 @@ const SchemeItem = ({
             transition={{ duration: 0.3 }}
             layout
           >
-            {enabled ? (
-              item.type === 'day' ? (
-                <>
-                  <label className="modal-label time-input">
-                    Start tijd:
-                    <input
-                      type="time"
-                      name={`startTime-${item.id}`}
-                      value={schemeSettings[item.id]?.startTime || ''}
-                      onChange={(e) =>
-                        handleInputChange(item.id, 'startTime', e.target.value)
-                      }
-                      required
-                    />
-                  </label>
-                  <label className="modal-label time-input">
-                    Eindtijd:
-                    <input
-                      type="time"
-                      name={`endTime-${item.id}`}
-                      value={schemeSettings[item.id]?.endTime || ''}
-                      onChange={(e) =>
-                        handleInputChange(item.id, 'endTime', e.target.value)
-                      }
-                      required
-                    />
-                  </label>
-                  {isSaveAttempted &&
-                    errors[item.id] &&
-                    Object.values(errors[item.id])
-                      .filter((error) => typeof error === 'string')
-                      .map((errorMsg, index) => (
-                        <span key={index} className="error-shifts">
-                          {errorMsg}
-                        </span>
-                      ))}
-                </>
-              ) : (
-                <>
-                  <label className="modal-label date-input">
-                    Start datum:
-                    <input
-                      type="date"
-                      name={`startDate-${item.id}`}
-                      value={schemeSettings[item.id]?.startDate || ''}
-                      onChange={(e) =>
-                        handleInputChange(item.id, 'startDate', e.target.value)
-                      }
-                      required
-                    />
-                  </label>
-                  <label className="modal-label date-input">
-                    Eind datum:
-                    <input
-                      type="date"
-                      name={`endDate-${item.id}`}
-                      value={schemeSettings[item.id]?.endDate || ''}
-                      onChange={(e) =>
-                        handleInputChange(item.id, 'endDate', e.target.value)
-                      }
-                      required
-                    />
-                  </label>
-                  {isSaveAttempted &&
-                    errors[item.id] &&
-                    Object.values(errors[item.id]).map((errorMsg, index) => (
+            {item.type === 'day' ? (
+              <>
+                <label className="modal-label time-input">
+                  Start tijd:
+                  <input
+                    type="time"
+                    name={`startTime-${item.id}`}
+                    value={schemeSettings[item.id]?.startTime || ''}
+                    onChange={(e) =>
+                      handleInputChange(item.id, 'startTime', e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                <label className="modal-label time-input">
+                  Eindtijd:
+                  <input
+                    type="time"
+                    name={`endTime-${item.id}`}
+                    value={schemeSettings[item.id]?.endTime || ''}
+                    onChange={(e) =>
+                      handleInputChange(item.id, 'endTime', e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                {isSaveAttempted &&
+                  errors[item.id] &&
+                  Object.values(errors[item.id])
+                    .filter((error) => typeof error === 'string')
+                    .map((errorMsg, index) => (
                       <span key={index} className="error-shifts">
                         {errorMsg}
                       </span>
                     ))}
-                </>
-              )
+              </>
             ) : (
-              <p className="disabled-message">Dit item is uitgeschakeld.</p>
+              <>
+                <label className="modal-label date-input">
+                  Start datum:
+                  <input
+                    type="date"
+                    name={`startDate-${item.id}`}
+                    value={schemeSettings[item.id]?.startDate || ''}
+                    onChange={(e) =>
+                      handleInputChange(item.id, 'startDate', e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                <label className="modal-label date-input">
+                  Eind datum:
+                  <input
+                    type="date"
+                    name={`endDate-${item.id}`}
+                    value={schemeSettings[item.id]?.endDate || ''}
+                    onChange={(e) =>
+                      handleInputChange(item.id, 'endDate', e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                {isSaveAttempted &&
+                  errors[item.id] &&
+                  Object.values(errors[item.id]).map((errorMsg, index) => (
+                    <span key={index} className="error-shifts">
+                      {errorMsg}
+                    </span>
+                  ))}
+              </>
             )}
 
-            {enabled && item.type === 'day' && (
+            {/* Render Shifts component if enabled and item type is 'day' */}
+            {item.type === 'day' && (
               <Shifts
                 itemId={item.id}
                 schemeSettings={schemeSettings}
