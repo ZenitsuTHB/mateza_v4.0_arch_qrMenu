9f8fe6afd22544584ed7ea06274b358f58cb9492
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-11-01 00:09:55 +0100

    Changed files: src/index.js Committed on: 11/1/2024, 12:09:55 AM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Providers from './Providers';
 import App from './App';
+
+window.isIframe = window !== window.parent;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
