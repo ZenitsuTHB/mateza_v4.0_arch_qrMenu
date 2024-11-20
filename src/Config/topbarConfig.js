343cdd9386c3fa6d4fe3fb9caecaa625b946127b
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-11-20 01:59:38 +0100

    Changed files: src/Config/topbarConfig.js Committed on: 11/20/2024, 1:59:38 AM

--- src/Config/topbarConfig.js
+++ src/Config/topbarConfig.js
@@ -1,10 +1,16 @@
-import { FaPlus, FaChartLine, FaLock, FaUser, FaCog } from 'react-icons/fa';
+import { FaPlus, FaChartLine, FaLock, FaUser, FaCog, FaPencilAlt } from 'react-icons/fa';
 
 const topBarConfig = [
   {
     label: 'Add',
     icon: FaPlus,
     path: '/new-reservation',
+  },
+
+  {
+    label: 'Account',
+    icon: FaPencilAlt,
+    path: '/account',
   },
 
   {
