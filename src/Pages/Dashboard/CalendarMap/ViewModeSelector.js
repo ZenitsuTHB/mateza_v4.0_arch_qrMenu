fdc5a87b22c725ac5c85f7191690556ef995d39f
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-11-26 00:54:47 +0100

    Release on 26-11-2024 at 00:54 CEST

--- src/Pages/Dashboard/CalendarMap/ViewModeSelector.js
+++ src/Pages/Dashboard/CalendarMap/ViewModeSelector.js
@@ -12,7 +12,7 @@ const ViewModeSelector = ({
   const viewModeButtonRef = useRef(null);
   const viewModeOptionsRef = useRef(null);
 
-  const viewModes = ['Algemeen', 'Bezettingsgraad', 'Bezettingspercentage', 'Voorspelling', 'Weer'];
+  const viewModes = ['Algemeen', 'Weer'];
 
   // Handle Click Outside for View Mode Options
   useEffect(() => {
