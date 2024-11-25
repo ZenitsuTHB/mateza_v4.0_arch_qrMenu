3a81e8b67bfa6855ca2fc68acf72b7394f902bb9
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-11-25 02:31:56 +0100

    Changed files: build.zip, src/Providers.js Committed on: 11/25/2024, 2:31:55 AM

--- src/Providers.js
+++ src/Providers.js
@@ -1,7 +1,7 @@
 // Providers.js
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { SearchProvider } from './Context/SearchContext';
 import store from './Redux/store';
@@ -10,11 +10,11 @@ import i18n from './i18n';
 const Providers = ({ children }) => (
   <I18nextProvider i18n={i18n}>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <SearchProvider>
           {children}
         </SearchProvider>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </I18nextProvider>
 );
