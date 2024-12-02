b562fe7c67b751c6ed11900e652bd1c8e6e8edc6
Thibault Van de Sompele <153604215+thibaultvandesompele@users.noreply.github.com>
2024-12-02 02:51:49 +0100

    Changed files: package.json, src/Pages/Uitzonderingen/constants.js Committed on: 12/2/2024, 2:51:49 AM

--- src/Pages/Uitzonderingen/constants.js
+++ src/Pages/Uitzonderingen/constants.js
@@ -1,7 +1,7 @@
 // src/Pages/Uitzonderingen/constants.js
 
 export const shifts = {
-	Ontbijt: { start: '04:00', end: '11:00' },
+	Ontbijt: { start: '07:00', end: '11:00' },
 	Lunch: { start: '11:00', end: '16:00' },
 	Diner: { start: '16:00', end: '23:00' },
   };
